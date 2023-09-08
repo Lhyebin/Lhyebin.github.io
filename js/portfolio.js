8aedcb5c25d2d10febf6a0797430c18d082071d6
Lhyebin <133613625+Lhyebin@users.noreply.github.com>
2023-09-08 17:50:53 +0900

    Add files via upload

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,28 +1,65 @@
 $(function () {
-
     $('.Main_content').fullpage({
-        anchors: ['main', 'w_sub01', 'w_sub02', 'w_sub03', 'w_sub04', 'w_sub05', 'vi_sub01', 'vi_sub02', 'vi_sub03', 'vi_sub04', 'vi_sub05', 'footer'],
-        //  스크롤 내릴때마다 한 풀페이지마다 주소url에 이름 변경하기
+        anchors: ['main', 'w_sub01', 'w_sub02', 'w_sub03', 'w_sub04', 'w_sub05', 'vi_sub01', 'vi_sub02', 'vi_sub03', 'vi_sub04', 'vi_sub05', 'profile'],
         navigation: false,
-        //  왼쪽에 몇번째 풀페이지인지 도트 표기 할지 안할지
         responsiveWidth: 700,
         scrollOverflow: false,
-        //line-height: 1에서 font-size가 box를 초과할 때 스크롤이 생기는 초기값을 false로 설정함.
-
         afterRender: function () {
             $('.Main_content .section').eq(0).addClass('on')
         },
-        afterLoad: function (link, idx) {
-            //  afterLoad : 풀페이지 페이지마다 왼쪽바 같이 연동되게
+        _afterLoad: function (link, idx) {
             console.log(link, idx);
             $('.Main_content .section').eq(idx - 1).addClass('on').siblings().removeClass('on');
-            if (idx == 1) {
-                // $('.header').addClass('on');
-                // $('.gnb').addClass('on');
-            } else {
-                // $('.header').removeClass('on');
-                // $('.gnb').removeClass('on');
+
+
+
+
+            // <<<<< header_top 최상단 올리는 버튼 설정>>>>
+            if (idx > 1) {
+                $('.header_top').css({ display: "block" });
             }
+            if (idx == 1) {
+                // 메인 첫 페이지에선 안보이게
+                $('.header_top').css({ display: "none" });
+            }
+
+
+
+
+            // <<<<<< dots list : 웹,비주얼 섹션별 설정 >>>>
+            $('.web_dots li').eq(idx - 2).addClass('on').siblings().removeClass('on');
+            $('.visual_dots li').eq(idx - 7).addClass('on').siblings().removeClass('on');
+
+            if (idx > 1 && idx < 7) {
+                $('.list1').css({ display: "block" });
+                $('.list2').css({ display: "none" });
+
+            } else {
+                $('.list1').css({ display: "none" });
+                $('.list2').css({ display: "block" });
+            }
+
+            if (idx == 1) {
+                // 메인 첫 페이지에선 안보이게
+                $('.list1').css({ display: "none" });
+                $('.list2').css({ display: "none" });
+            }
+            if (idx == 12) {
+                // 프로필 마지막 페이지에선 안보이게
+                $('.list1').css({ display: "none" });
+                $('.list2').css({ display: "none" });
+            }
+        },
+
+
+
+
+
+        get afterLoad() {
+            return this._afterLoad;
+        },
+        set afterLoad(value) {
+            this._afterLoad = value;
         },
     });
 
@@ -31,7 +68,7 @@ $(function () {
 
 
 
-    // <<<< 헤더 : 메뉴바 아이콘 >>>>>>>>
+    // <<<< 메뉴바 아이콘 >>>>>>>>
     // 메뉴바 클릭했을때 나왔다들어갔다 toggle
     $('.header_bar').on('click', function () {
         $('.header_gnb').toggleClass('on');
@@ -58,8 +95,5 @@ $(function () {
     //         $(this).next().slideToggle();
     //         $(this).parent().siblings().find('.sub').slideUp();
     //     })
-
-
-
 
 })
